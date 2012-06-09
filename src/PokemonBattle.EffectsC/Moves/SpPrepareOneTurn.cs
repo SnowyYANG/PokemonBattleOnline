dd11de693ef68263776a8f8b382bb8ef41ddf153
Snowy_cp <SND\Snowy_cp>
2012-06-09 04:37:44 -0700

    fix last check missing file

---
+++ src/PokemonBattle.EffectsC/Moves/SpPrepareOneTurn.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LightStudio.PokemonBattle.Data;
+using LightStudio.PokemonBattle.Game;
+
+namespace LightStudio.PokemonBattle.Effects.Moves
+{
+  class Leap : AttackMoveE
+  {
+    private CoordY y;
+    
+    public Leap(int id, CoordY y)
+      : base(id)
+    {
+    }
+    
+    protected override bool PrepareOneTurn(PokemonProxy pm)
+    {
+      if (base.PrepareOneTurn(pm))
+      {
+        pm.OnboardPokemon.CoordY = y;
+        return true;
+      }
+      return false;
+    }
+  }
+  class SolarBeam : AttackMoveE
+  {
+    public SolarBeam()
+      : base(76)
+    {
+    }
+
+    protected override bool PrepareOneTurn(PokemonProxy pm)
+    {
+      return base.PrepareOneTurn(pm) && pm.Controller.GetAvailableWeather() != Weather.IntenseSunlight;
+    }
+  }
+  class SkullBash : AttackMoveE
+  {
+    public SkullBash()
+      : base(130)
+    {
+    }
+    protected override bool PrepareOneTurn(PokemonProxy pm)
+    {
+      if (base.PrepareOneTurn(pm))
+      {
+        pm.ChangeLv7D(pm, 0, 1);
+        return true;
+      }
+      return false;
+    }
+  }
+}
