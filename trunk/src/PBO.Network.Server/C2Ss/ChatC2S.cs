8c4c34d0f6070449ae9263b4ac690382580d85ba
Snowy_cp <SND\Snowy_cp>
2013-10-12 07:50:47 -0700

    fix last checkin

---
+++ trunk/src/PBO.Network.Server/C2Ss/ChatC2S.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace PokemonBattleOnline.Network.C2Ss
+{
+  [DataContract(Namespace = PBOMarks.JSON)]
+  internal class ChatC2S : Commands.ChatC2S
+  {
+
+  }
+}
