10db8d5c5757362653a5ed5b7a10bab4f018869f
Snowy_cp <SND\Snowy_cp>
2014-01-12 23:25:25 -0800

    server ui?

--- trunk/src/PBOServer/Program.cs
+++ trunk/src/PBOServer/Program.cs
@@ -10,27 +10,66 @@ namespace PokemonBattleOnline.PBO.Server
 {
   class Program
   {
+    static void PreLog(string message)
+    {
+      Console.Write(DateTime.Now.ToString("(hh:mm:ss) "));
+      Console.Write(message);
+      Console.Write("... ");
+    }
     static int Main(string[] args)
     {
-      using (var pack = new ZipData("..\\res\\rom.zip"))
+      Console.Title = PBOMarks.TITLE + " Server";
+      try
       {
-        RomData.Load(pack, "/rom.xml");
-        LearnList.Load(pack, "/learnset");
-      }
-      PBOServer.NewServer(PBOMarks.DEFAULT_PORT);
-      var server = PBOServer.Current;
-      server.Start();
-      Thread t = new Thread(TaskbarIcon.Init);
-      t.Start();
+        PreLog("Loading data");
+        using (var pack = new ZipData("..\\res\\rom.zip"))
+        {
+          RomData.Load(pack, "/rom.xml");
+          LearnList.Load(pack, "/learnset");
+        }
+        Console.WriteLine("OK!");
 
-    LOOP:
-      var line = Console.ReadLine();
-      if (line.Equals("quit", StringComparison.CurrentCultureIgnoreCase))
+        PreLog("Opening server");
+        PBOServer.NewServer(PBOMarks.DEFAULT_PORT);
+        PBOServer.Current.Start();
+        Console.WriteLine("OK!");
+
+        PreLog("Initing taskbar notify icon");
+        new Thread(TaskbarIcon.Init).Start();
+        Console.WriteLine("OK!");
+
+        Console.WriteLine();
+        Console.WriteLine(@"Server is ready. To close server, input ""quit"" or close this window. To hide this window, click the notify icon in task.");
+        Console.WriteLine();
+
+        new Thread(() =>
+          {
+            while (true)
+            {
+              Thread.Sleep(500);
+              Console.WriteLine("Test");
+            }
+          }).Start();
+
+      LOOP:
+        Console.Write("Input command: ");
+        var line = Console.ReadLine();
+        if (line.Equals("quit", StringComparison.CurrentCultureIgnoreCase))
+        {
+          PBOServer.Current.Dispose();
+          TaskbarIcon.Close();
+          return 0;
+        }
+        goto LOOP;
+      }
+      catch (Exception e)
       {
-        TaskbarIcon.Close();
+        Console.Write("Error: ");
+        Console.WriteLine(e.ToString());
+        Console.WriteLine("Press any key to quit...");
+        Console.ReadKey(false);
         return 0;
       }
-      goto LOOP;
     }
   }
 }
