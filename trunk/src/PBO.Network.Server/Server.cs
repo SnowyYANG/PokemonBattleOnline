bf36684d00d8763ef547474397d14fec99863b83
adwrddbg_cp <SND\adwrddbg_cp>
2014-02-05 00:09:44 -0800

    list功能更新

--- trunk/src/PBO.Network.Server/Server.cs
+++ trunk/src/PBO.Network.Server/Server.cs
@@ -54,7 +54,7 @@ namespace PokemonBattleOnline.Network
     {
         foreach (ServerUser user in Users.Values)
         {
-            Console.WriteLine(user.User.Name);
+            Console.WriteLine(user.User.Name + "" + user.Network.EndPoint.Address.ToString());
         }
     }
     public void BanIp(IPAddress ip)
