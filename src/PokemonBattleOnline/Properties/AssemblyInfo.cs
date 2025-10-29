6adba3914bb7f9d47ee9ecb81766aa8ebfacb085
Snowy <snowyyang@outlook.com>
2025-10-29 18:30:53 +0800

    Move assembly metadata from AssemblyInfo.cs to .csproj
    Assembly attributes such as title, company, product, copyright, and version information were removed from Properties/AssemblyInfo.cs and added to PokemonBattleOnline.csproj. This centralizes assembly metadata in the project file, streamlining configuration and version management. No functional code changes were made.

--- src/PokemonBattleOnline/Properties/AssemblyInfo.cs
+++ src/PokemonBattleOnline/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("PokemonBattleOnline")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("http://pbo.codeplex.com/")]
-[assembly: AssemblyProduct("PokemonBattleOnline")]
-[assembly: AssemblyCopyright("http://pbo.codeplex.com/license")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7e8f5db9-3a6b-4243-9852-6dbed81bf95a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.1.0.0")]
-[assembly: AssemblyFileVersion("6.1.0.0")]
