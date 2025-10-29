ebff4f7846f774b22c8d2a1bc3180e95fdf7cd17
Snowy <snowyyang@outlook.com>
2025-10-29 18:32:44 +0800

    Move assembly metadata from AssemblyInfo.cs to .csproj
    Assembly attributes such as title, company, product, copyright, and version information were removed from Properties/AssemblyInfo.cs and added to PBO.Game.Host.csproj. This centralizes assembly metadata in the project file, streamlining configuration and reducing redundancy. No functional code changes were made.

--- src/PBO.Game.Host/Properties/AssemblyInfo.cs
+++ src/PBO.Game.Host/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("PBO.Game.Host")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("http://pbo.codeplex.com/")]
-[assembly: AssemblyProduct("PBO.Game.Host")]
-[assembly: AssemblyCopyright("http://pbo.codeplex.com/license")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a0ef7609-1ec9-45c3-b55b-93b4b5e00d48")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.1.0.0")]
-[assembly: AssemblyFileVersion("6.1.0.0")]
